1244580733a93bf71dc304936621f6b4b3bfb481
Persiuskane <phoenixstudioproduct@gmail.com>
2026-04-04 00:05:30 +0530

    Code the game

--- Tic Tac Toe/Vanila Build/js/app.js
+++ Tic Tac Toe/Vanila Build/js/app.js
@@ -1,3 +1,6 @@
+import Store from "./store.js";
+import View from "./view.js";
+
 // Namespace
 const App = {
   $: {
@@ -6,10 +9,49 @@ const App = {
     resetBtn: document.querySelector('[data-id="reset-btn"]'),
     newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
     squares: document.querySelectorAll('[data-id="square"]'),
+    model: document.querySelector('[data-id="modal"]'),
+    modelTxt: document.querySelector('[data-id="modal-text"]'),
+    modelBtn: document.querySelector('[data-id="modal-btn"]'),
+    turn: document.querySelector('[data-id="turn"]'),
+    turnIcon: document.querySelector('[data-id="turn-icon"]'),
+    turntext: document.querySelector('[data-id="turn-text"]'),
   },
 
   state: {
-    currentPlayer: 1,
+    moves: [],
+  },
+
+  getGameStates(moves) {
+    const p1Moves = moves
+      .filter((move) => move.playerId === 1)
+      .map((move) => +move.squareId);
+    const p2Moves = moves
+      .filter((move) => move.playerId === 2)
+      .map((move) => +move.squareId);
+    const winningPatterns = [
+      [1, 2, 3],
+      [1, 5, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 5, 7],
+      [3, 6, 9],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+
+    let winner = null;
+
+    winningPatterns.forEach((pattern) => {
+      const p1Wins = pattern.every((v) => p1Moves.includes(v));
+      const p2Wins = pattern.every((v) => p2Moves.includes(v));
+      if (p1Wins) winner = 1;
+      if (p2Wins) winner = 2;
+    });
+
+    return {
+      status: moves.length === 9 || winner != null ? "complete" : "in-progress", // in-progress | complete
+      winner, // 1 | 2 | Null
+    };
   },
 
   init() {
@@ -32,30 +74,145 @@ const App = {
       console.log("New Round");
     });
 
+    App.$.modelBtn.addEventListener("click", (event) => {
+      App.state.moves = [];
+      App.$.squares.forEach((square) => square.replaceChildren());
+      App.$.model.classList.toggle("hidden");
+    });
+
     App.$.squares.forEach((square) => {
       square.addEventListener("click", (event) => {
-        console.log(`Clicked ${event.target.id}`);
-        console.log(`Current Player is ${App.state.currentPlayer}`);
+        // Check If allready played
+        const hasMove = (squareId) => {
+          const existingMove = App.state.moves.find(
+            (move) => move.squareId === squareId,
+          );
+          return existingMove !== undefined;
+        };
 
-        if (square.hasChildNodes()) {
+        if (hasMove(+square.id)) {
           return;
         }
+
+        // Create a Icon Element Determine the Player
         const icon = document.createElement("i");
 
-        if (App.state.currentPlayer === 1) {
+        const lastMove = App.state.moves.at(-1);
+        getOppersitePlayer = (playerId) => (playerId === 1 ? 2 : 1);
+        const currentPlayer =
+          App.state.moves.length === 0
+            ? 1
+            : getOppersitePlayer(lastMove.playerId);
+
+        const nextPlayer = getOppersitePlayer(currentPlayer);
+        App.$.turntext.textContent = `Player ${nextPlayer}, you're up!`;
+
+        if (currentPlayer === 1) {
           icon.classList.add("fa-solid", "fa-x", "turquoise");
+          App.$.turnIcon.classList.replace("fa-x", "fa-o");
+          App.$.turn.classList.replace("turquoise", "yellow");
         } else {
           icon.classList.add("fa-solid", "fa-o", "yellow");
+          App.$.turnIcon.classList.replace("fa-o", "fa-x");
+          App.$.turn.classList.replace("yellow", "turquoise");
         }
 
-        App.state.currentPlayer = App.state.currentPlayer === 1 ? 2 : 1;
+        App.state.moves.push({
+          squareId: +square.id,
+          playerId: currentPlayer,
+        });
+
+        // App.state.currentPlayer = currentPlayer === 1 ? 2 : 1;
         square.replaceChildren(icon);
 
-        // <i class="fa-solid fa-x turquoise"></i>
-        // <i class="fa-solid fa-o yellow"></i>
+        console.log(App.state);
+
+        // Check If their a winner or a Tie
+        const game = App.getGameStates(App.state.moves);
+        if (game.status === "complete") {
+          App.$.model.classList.toggle("hidden");
+          let msg = "";
+          if (game.winner) {
+            msg = `player ${game.winner} has won.`;
+          } else {
+            msg = "Tie! Game";
+          }
+
+          App.$.modelTxt.textContent = msg;
+        }
       });
     });
   },
 };
 
-window.addEventListener("load", App.init);
+const players = [
+  {
+    id: 1,
+    name: "Player 1",
+    iconClass: "fa-x",
+    colorClass: "turquoise",
+  },
+  {
+    id: 2,
+    name: "Player 2",
+    iconClass: "fa-o",
+    colorClass: "yellow",
+  },
+];
+
+function init() {
+  const view = new View();
+  const store = new Store("live-t3-game", players);
+
+  function initView() {
+    view.closeAll();
+    view.clearMoves();
+    view.setTurnIndicator(store.game.currentPlayer);
+    view.updateScoreBoard(
+      store.stats.playerWithStats[0].wins,
+      store.stats.playerWithStats[1].wins,
+      store.stats.ties,
+    );
+    view.initializeMove(store.game.moves);
+  }
+
+  initView();
+  window.addEventListener("storage", () => {
+    initView();
+  });
+
+  view.bindGameResetEvent((event) => {
+    store.reset();
+    initView();
+    console.log(store.stats);
+  });
+
+  view.bindNewRoundBtnEvent((event) => {
+    store.newRound();
+    initView();
+  });
+
+  view.bindPlayerMoveEvent((square) => {
+    const existingMoves = store.game.moves.find(
+      (move) => move.squareId === +square.id,
+    );
+
+    if (existingMoves) return;
+    // console.log(store.game.currentPlayer);
+
+    view.handlePlayerMove(square, store.game.currentPlayer);
+    store.playerMove(+square.id);
+
+    if (store.game.status.isComplete) {
+      const msg = store.game.status.winner
+        ? `${store.game.status.winner.name} has won.`
+        : "Tie!";
+      view.openModel(msg);
+      return;
+    }
+
+    view.setTurnIndicator(store.game.currentPlayer);
+  });
+}
+
+window.addEventListener("load", init);
---
+++ Tic Tac Toe/Vanila Build/js/view.js
@@ -0,0 +1,138 @@
+export default class View {
+  $ = {};
+  $$ = {};
+
+  constructor() {
+    this.$.menu = this.#qs('[data-id="menu"]');
+    this.$.menuBtn = this.#qs('[data-id="menu-button"]');
+    this.$.menuItems = this.#qs('[data-id="menu-items"]');
+    this.$.resetBtn = this.#qs('[data-id="reset-btn"]');
+    this.$.newRoundBtn = this.#qs('[data-id="new-round-btn"]');
+    this.$.model = this.#qs('[data-id="modal"]');
+    this.$.modelTxt = this.#qs('[data-id="modal-text"]');
+    this.$.modelBtn = this.#qs('[data-id="modal-btn"]');
+    this.$.turn = this.#qs('[data-id="turn"]');
+    this.$.p1Wins = this.#qs('[data-id="p1-wins"]');
+    this.$.p2Wins = this.#qs('[data-id="p2-wins"]');
+    this.$.ties = this.#qs('[data-id="ties"]');
+    // this.$.turnIcon = this.#qs('[data-id="turn-icon"]');
+    // this.$.turntext = this.#qs('[data-id="turn-text"]');
+
+    this.$$.squares = this.#qsAll('[data-id="square"]');
+
+    // Ui Only Event Listners
+    this.$.menuBtn.addEventListener("click", (event) => {
+      this.#toggleMenu();
+    });
+  }
+
+  /**
+   * Register All Utility Methods
+   */
+
+  bindGameResetEvent(handler) {
+    this.$.resetBtn.addEventListener("click", handler);
+    this.$.modelBtn.addEventListener("click", handler);
+  }
+
+  bindNewRoundBtnEvent(handler) {
+    this.$.newRoundBtn.addEventListener("click", handler);
+  }
+
+  bindPlayerMoveEvent(handler) {
+    this.$$.squares.forEach((square) => {
+      square.addEventListener("click", () => handler(square));
+    });
+  }
+
+  /**
+   * DOM Helper Methods
+   */
+
+  updateScoreBoard(p1Wins, p2Wins, ties) {
+    this.$.p1Wins.innerText = `${p1Wins} wins`;
+    this.$.p2Wins.innerText = `${p2Wins} wins`;
+    this.$.ties.innerText = `${ties}`;
+  }
+
+  openModel(msg) {
+    this.$.model.classList.remove("hidden");
+    this.$.modelTxt.textContent = msg;
+  }
+
+  closeAll() {
+    this.#closeModel();
+    this.#closeMenu();
+  }
+
+  clearMoves() {
+    this.$$.squares.forEach((square) => {
+      square.replaceChildren();
+    });
+  }
+
+  initializeMove(moves) {
+    this.$$.squares.forEach((square) => {
+      const existingMoves = moves.find((move) => move.squareId === +square.id);
+
+      if (existingMoves) {
+        this.handlePlayerMove(square, existingMoves.player);
+      }
+    });
+  }
+
+  #closeModel() {
+    this.$.model.classList.add("hidden");
+  }
+
+  #closeMenu() {
+    this.$.menuItems.classList.add("hidden");
+    this.$.menuBtn.classList.remove("border");
+    const icon = this.#qs("i", this.$.menu);
+
+    icon.classList.add("fa-chevron-down");
+    icon.classList.remove("fa-chevron-up");
+  }
+
+  #toggleMenu() {
+    this.$.menuItems.classList.toggle("hidden");
+    this.$.menuBtn.classList.toggle("border");
+    const icon = this.#qs("i", this.$.menu);
+
+    icon.classList.toggle("fa-chevron-down");
+    icon.classList.toggle("fa-chevron-up");
+  }
+
+  setTurnIndicator(player) {
+    const turn = this.$.turn;
+    const turnIcon = document.createElement("i");
+    const turnText = document.createElement("p");
+
+    turnIcon.classList.add("fa-solid", player.iconClass, player.colorClass);
+    turnText.classList.add(player.colorClass);
+
+    turnText.innerText = `${player.name}, you're up!`;
+
+    turn.replaceChildren(turnIcon, turnText);
+  }
+
+  handlePlayerMove(squareEl, player) {
+    const icon = document.createElement("i");
+    icon.classList.add("fa-solid", player.iconClass, player.colorClass);
+    squareEl.replaceChildren(icon);
+  }
+
+  #qs(selector, parent) {
+    const el = parent
+      ? parent.querySelector(selector)
+      : document.querySelector(selector);
+    if (!el) throw new Error("Could not find elements");
+    return el;
+  }
+
+  #qsAll(selector) {
+    const elList = document.querySelectorAll(selector);
+    if (!elList) throw new Error("Could not find elements");
+    return elList;
+  }
+}
